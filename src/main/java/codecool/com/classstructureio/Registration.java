881e498775c413b6c747ddba9288524e6b9cf666
bartaodi <barta.kod@gmail.com>
2021-05-14 10:10:30 +0200

    Registration class implemented

--- src/main/java/codecool/com/classstructureio/Registration.java
+++ src/main/java/codecool/com/classstructureio/Registration.java
@@ -1,5 +1,20 @@
 package codecool.com.classstructureio;
 
+import java.util.Scanner;
+
 public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérem adja meg a nevét: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Kérem adja meg az email címét: ");
+        String email = scanner.nextLine();
+
+        System.out.println("Az ön regisztrációs adatai: ");
+        System.out.println("Felhasználónév: " + userName);
+        System.out.println("Email cím: " + email);
+    }
 
 }
